c182cbeb4c881854dc499c88d2a35588bab3847c
kernel1994 <almostlover@outlook.com>
2022-12-06 11:58:12 +0800

    Update options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -29,11 +29,13 @@ vim.opt.termguicolors = true
 vim.opt.list = true
 vim.opt.listchars = 'eol:↵,trail:·,tab:>-,nbsp:␣'
 
--- :h last-position-jump
 vim.cmd [[
     autocmd BufRead * autocmd FileType <buffer> ++once
-        \ if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif
+    \ if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif
 ]]
+
+require('mini.cursorword').setup({ delay = 100 })
+require('mini.comment').setup()
 
 require('toggleterm').setup{}
 require('nvim-surround').setup{}
@@ -51,18 +53,18 @@ require'nvim-treesitter.configs'.setup {
 -- You dont need to set any of these options. These are the default ones. Only
 -- the loading is important
 require('telescope').setup {
-  extensions = {
-    fzf = {
-      fuzzy = true,                    -- false will only do exact matching
-      override_generic_sorter = true,  -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = 'smart_case',        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
-    },
-    frecency = {
-        show_scores = true,
+    extensions = {
+        fzf = {
+            fuzzy = true,                    -- false will only do exact matching
+            override_generic_sorter = true,  -- override the generic sorter
+            override_file_sorter = true,     -- override the file sorter
+            case_mode = 'smart_case',        -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+        },
+        frecency = {
+            show_scores = true,
+        }
     }
-  }
 }
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
