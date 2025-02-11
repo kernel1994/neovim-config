518dfc845ea5565ff11d976e757504094ba398a4
kernel1994 <9877756+kernel1994@users.noreply.github.com>
2025-02-11 08:59:51 +0000

    config osc52
    Neovim has natively supported OSC-52 protocol since 0.10.0

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,16 +1,32 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.o.clipboard = "unnamedplus"
 
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+-- https://www.cnblogs.com/sxrhhh/p/18234652/neovim-copy-anywhere
+-- https://github.com/neovim/neovim/issues/28611
+function my_paste(reg)
+  return function(lines)
+      -- 返回 "" 寄存器的内容，用来作为 p 操作符的粘贴物
+      local content = vim.fn.getreg('"')
+      return vim.split(content, '\n')
+  end
+end
+
+if os.getenv('SSH_TTY') == nil then
+  -- 当前环境为本地环境，也包括 wsl
+  vim.opt.clipboard:append("unnamedplus")
+else
+  vim.opt.clipboard:append("unnamedplus")
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      -- 小括号里面的内容可能是毫无意义的，但是保持原样可能看起来更好一点
+      ["+"] = my_paste("+"),
+      ["*"] = my_paste("*"),
   },
 }
+end
